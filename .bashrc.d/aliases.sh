d4552ac7b55f3240cf4a1081b650723bbf2b0d85
Nathanael Rolim <natanael.rolim@hotmail.com>
2025-03-11 19:45:53 -0300

    Change Ollama instance to quadlet

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,21 +1,30 @@
 # Aliases files
+
 alias ll='ls -lh'
+
 alias untar='tar xvzf'
 
+# General
+
+alias open='xdg-open'
+
 # Tools
+
 alias dbx='distrobox'
 
-# Desktop Environment
-if [[ -v XDG_SESSION_DESKTOP ]]; then
-	alias open='xdg-open'
-fi
-
 # GNOME exclusive
+
 if [[ -v XDG_SESSION_DESKTOP ]] && [[ $XDG_SESSION_DESKTOP == "gnome" ]]; then
 	alias trash='gio trash'
 fi
 
 # Git
+
 alias gaa='git add .'
 alias gcm='git commit -m'
 alias gpo='git push -u origin main'
+
+
+# Ollama
+alias ollama-start='systemctl --user start ollama.service'
+alias ollama-stop='systemctl --user stop ollama.service'
