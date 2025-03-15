c27968ba023f3ab6497829fb0cdd3258d8d248de
Nathanael Rolim <natanael.rolim@hotmail.com>
2025-03-15 19:32:50 -0300

    Add more aliases

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,28 +1,27 @@
 # Aliases files
 
+# General
 alias ll='ls -lh'
-
 alias untar='tar xvzf'
 
-# General
-
-alias open='xdg-open'
-
 # Tools
-
 alias dbx='distrobox'
 
-# GNOME exclusive
+# Desktop
+if [[ -v XDG_SESSION_DESKTOP ]]; then
+	alias open='xdg-open'
+fi
 
+# GNOME exclusive
 if [[ -v XDG_SESSION_DESKTOP ]] && [[ $XDG_SESSION_DESKTOP == "gnome" ]]; then
 	alias trash='gio trash'
 fi
 
 # Git
-
 alias gaa='git add .'
 alias gcm='git commit -m'
-alias gpo='git push -u origin main'
+alias gpo='git push -u origin'
+alias gpr='git pull --rebase'
 
 
 # Ollama
