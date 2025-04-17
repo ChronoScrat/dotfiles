4f041c1862d3ab6f5aaf449934fa4b0b0abb93da
Nathanael Rolim <natanael.rolim@hotmail.com>
2025-04-17 00:33:32 -0300

    Improve general starship config

--- .bashrc.d/oh-my-posh.sh
+++ .bashrc.d/oh-my-posh.sh
@@ -1,6 +1,6 @@
 # Start Oh-My-Posh on bash
 # I've disabled this in favour of starship, but I'm keeping it for backup
 
-#if [ command -v oh-my-posh 2>&1 >/dev/null ]; then
+#if (command -v oh-my-posh 2>&1 >/dev/null); then
 #    eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/catppuccin_mocha.omp.json)"  
 #fi
